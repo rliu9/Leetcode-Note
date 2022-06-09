92ecd940904ac359bbddd90fd4f016f7c22d21b2
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-06-09 15:35:25 -0400

    Create  141. Linked List Cycle.py

---
+++ Cycle | FastSlow/141. Linked List Cycle/141. Linked List Cycle.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        """
+        seen = set()
+        while head:
+            if head in seen:
+                return True
+            seen.add(head)
+            head = head.next
+        return False
+        
+        # time complexity: O(n)
+        # space complexity: O(n)
+    
+        """
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:return True
+        return False
+    
+    # time complexity: O(n)
+    # space complexity: O(1)
