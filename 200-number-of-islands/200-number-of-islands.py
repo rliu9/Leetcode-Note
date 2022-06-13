b2c154dd12218cbacecb7d43ddee333ef300205e
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-06-13 11:34:38 -0400

    Time: 693 ms (7.28%), Space: 16.1 MB (99.65%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,6 +1,7 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        row,col,res = len(grid),len(grid[0]),0
+        
+        """row,col,res = len(grid),len(grid[0]),0
         def dfs(i,j):
             if i >= row or j >= col or i < 0 or j < 0 or grid[i][j] != '1':
                 return
@@ -13,7 +14,26 @@ class Solution:
                 if grid[i][j] == '1':
                     dfs(i,j)
                     res += 1
-        return res
+        return res"""
     
     # time complexity: O(M*N)
     # space complexity: O(M*N)
+    
+        row,col,count = len(grid),len(grid[0]),0
+        def bfs(q):
+            while q:
+                for _ in range(len(q)):
+                    x,y = q.popleft()
+                    for dx,dy in [(1,0),(-1,0),(0,1),(0,-1)]:
+                        xx,yy = x+dx,y+dy
+                        if xx < 0 or yy < 0 or xx == row or yy == col or grid[xx][yy]=='0':
+                            continue
+                        grid[xx][yy] = '0'
+                        q.append((xx,yy))
+                        
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == '1':
+                    bfs(deque([(i,j)]))
+                    count += 1    
+        return count
