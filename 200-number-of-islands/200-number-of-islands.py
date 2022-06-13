406ebae6a64176702baa7a403998a67ba48f4b98
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-06-13 11:42:02 -0400

    Time: 428 ms (51.32%), Space: 16.3 MB (79.97%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,7 +1,9 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        """row,col,res = len(grid),len(grid[0]),0
+        """
+        DFS:
+        row,col,res = len(grid),len(grid[0]),0
         def dfs(i,j):
             if i >= row or j >= col or i < 0 or j < 0 or grid[i][j] != '1':
                 return
@@ -14,11 +16,13 @@ class Solution:
                 if grid[i][j] == '1':
                     dfs(i,j)
                     res += 1
-        return res"""
+        return res
+        """
     
     # time complexity: O(M*N)
     # space complexity: O(M*N)
     
+        #BFS: 
         row,col,count = len(grid),len(grid[0]),0
         def bfs(q):
             while q:
@@ -37,3 +41,6 @@ class Solution:
                     bfs(deque([(i,j)]))
                     count += 1    
         return count
+    
+    # time complexity: O(M*N)
+    # Maximum siblings in queue will be min(M, N). So space complexity is min(M,N)
