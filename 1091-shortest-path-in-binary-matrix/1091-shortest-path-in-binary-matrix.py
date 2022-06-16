c65962405f9ac1e60ad016fe27b78741f74f81db
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-06-15 20:26:56 -0400

    Time: 703 ms (79.51%), Space: 14.1 MB (99.62%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,32 +1,25 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        max_row,max_col = len(grid)-1,len(grid[0])-1
-        directions=[
-            (1,0),(-1,0),(0,1),(0,-1),(-1,-1),(-1,1),(1,-1),(1,1)
-        ]
-        def get_neighbours(row,col):
-            for r,c in directions:
-                rr,cc = r+row,c+col
-                if not (0<=rr<=max_row and 0<=cc<=max_col):
-                    continue
-                if grid[rr][cc] != 0:
-                    continue
-                yield (rr,cc)
-            
-        if grid[0][0] != 0 or grid[max_row][max_col] != 0:
+        row_len, col_len = len(grid)-1, len(grid[0])-1
+        if grid[0][0] != 0 or grid[row_len][col_len] != 0:
             return -1
-        q = deque()
-        q.append((0,0))
+        directions = [(1,0),(-1,0),(0,1),(0,-1),(-1,-1),(-1,1),(1,-1),(1,1)]
+        q = deque([(0,0)])
         grid[0][0] = 1
         while q:
-            row,col = q.popleft()
-            distance = grid[row][col]
-            if (row,col) == (max_row,max_col):return distance
-            for row_neighbour,col_neighbour in get_neighbours(row,col):
-                grid[row_neighbour][col_neighbour] = distance + 1
-                q.append((row_neighbour,col_neighbour))
+            for _ in range(len(q)):
+                r,c = q.popleft()
+                distance = grid[r][c]
+                if r == row_len and c == col_len:return distance
+                for dr,dc in directions:
+                    rr,cc = r+dr, c+dc
+                    if rr < 0 or cc < 0 or rr > row_len or cc > col_len:
+                        continue
+                    if grid[rr][cc] != 0:
+                        continue
+                    grid[rr][cc] = distance + 1
+                    q.append((rr,cc))
         return -1
-    
     # time complexity: O(n)
     # space complexity: o(n)
     
