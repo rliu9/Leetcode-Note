e354fccb0c89d46bd0bfb7b80e08537c63fdc0d6
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-06-18 02:35:45 -0400

    Time: 993 ms (27.03%), Space: 27.4 MB (18.12%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.py
@@ -1,11 +1,14 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        """
+        85% and 96%
+
         if k==0:return False
         i = 0
         d = defaultdict(int)
         for j in range(len(nums)):
             if nums[j] in d:
-                    return True
+                return True
             if j - i == k:
                 if d[nums[i]] == 1:
                     del d[nums[i]]
@@ -13,5 +16,19 @@ class Solution:
                     d[nums[i]] -= 1
                 i += 1
             d[nums[j]] += 1
+        return False"""
+        
+        d = {}
+        for i,j in enumerate(nums):
+            if j in d and i - d[j] <= k:
+                return True
+            d[j] = i
         return False
+    
+    # O(n)
+    # O(n)
+        
+    
+        
+    
                 
