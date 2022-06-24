e6f65313719f894f3d277d8517c30730a28b4c64
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-06-24 14:37:07 -0400

    Time: 1378 ms (20.91%), Space: 17.9 MB (45.03%) - LeetHub

--- 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
@@ -1,11 +1,18 @@
 class Solution:
     def minSwaps(self, data: List[int]) -> int:
         window_size = sum(data)
-        num_ones = sum(data[0:window_size])
+        num_ones = sum(data[:window_size])
         max_ones = num_ones
         for i in range(window_size, len(data)):
-            j = i - window_size
-            num_ones -= data[j]
-            num_ones += data[i]
-            max_ones = max(max_ones, num_ones)
+            j = i-window_size
+            num_ones = num_ones + data[i] - data[j]
+            max_ones = max(num_ones, max_ones)
         return window_size - max_ones
+    
+    """
+    Idea:
+    Number of all 1's - max_ones(contains most 1's with window size) = minimum number of swaps 
+    """
+    
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
