56741f94fac90f37457a1d67846d9efc6705b3a6
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-06-25 16:17:39 -0400

    Time: 191 ms (70.30%), Space: 17.7 MB (35.99%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -11,8 +11,8 @@ class Solution:
             dp[0][j] = j
         for i in range(1, n + 1):
             for j in range(1, m + 1):
-                left = dp[i - 1][j] + 1
-                down = dp[i][j - 1] + 1
+                down = dp[i - 1][j] + 1
+                left = dp[i][j - 1] + 1
                 left_down = dp[i - 1][j - 1] 
                 if word1[i - 1] != word2[j - 1]:
                     left_down += 1
