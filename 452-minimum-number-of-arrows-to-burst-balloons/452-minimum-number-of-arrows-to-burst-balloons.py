b20c18d12b101020e95702f828b4c51edc3cfe2c
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-06-27 17:29:06 -0400

    Time: 2088 ms (37.99%), Space: 60.2 MB (7.38%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,18 +1,23 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        if len(points) == 0: return 0
-        points = sorted(points, key=lambda x: x[0])
-
-        start = points[0][0]
-        end = points[0][1]
-        res = 0
-
-        for point in points:
-            if point[0] <= end:
-                start = max(point[0], start)
-                end = min(point[1], end)
+        if not points:return 0
+        points = sorted(points, key=lambda x:x[0])
+        start,end,res = points[0][0],points[0][1],0
+        for s,e in points:
+            if s <= end:
+                start = max(start, s)
+                end = min(end, e)
             else:
                 res += 1
-                start = point[0]
-                end = point[1]
-        return res + 1
+                start = s
+                end = e
+        return res+1
+    
+    # Time complexity: O(n logn)
+    # Space complexity: O(n)
+    
+if __name__ == '__main__':
+    s = Solution()
+    assert s.findMinArrowShots([[10,16],[2,8],[1,6],[7,12]]) == 2
+    assert s.findMinArrowShots([[1,2],[3,4],[5,6],[7,8]]) == 4
+    assert s.findMinArrowShots([[1,2],[2,3],[3,4],[4,5]]) == 2
