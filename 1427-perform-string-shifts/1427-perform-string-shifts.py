005e4aec6f18bca4fcd037e1f56b1037b5650d15
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-06-30 15:34:23 -0400

    Time: 44 ms (66.88%), Space: 13.9 MB (20.85%) - LeetHub

---
+++ 1427-perform-string-shifts/1427-perform-string-shifts.py
@@ -0,0 +1,11 @@
+class Solution:
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        move = 0
+        for direction, moves in shift:
+            if direction == 0:
+                move -= moves
+            else:
+                move += moves
+        move %= len(s)
+        return s[-move:] + s[:-move]
+                
