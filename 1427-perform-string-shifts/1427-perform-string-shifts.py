c4857b50e615603eb60fc734acb1cfafc0bc2d58
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-06-30 23:00:39 -0400

    Time: 63 ms (19.94%), Space: 13.9 MB (21.22%) - LeetHub

--- 1427-perform-string-shifts/1427-perform-string-shifts.py
+++ 1427-perform-string-shifts/1427-perform-string-shifts.py
@@ -3,9 +3,9 @@ class Solution:
         move = 0
         for direction, moves in shift:
             if direction == 0:
-                move -= moves
-            else:
                 move += moves
+            else:
+                move -= moves
         move %= len(s)
-        return s[-move:] + s[:-move]
+        return s[move:] + s[:move]
                 
