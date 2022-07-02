263a18734d45512bc4e7bc4d534ad0cfabefb144
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-07-02 00:21:02 -0400

    Time: 87 ms (71.11%), Space: 27.7 MB (14.68%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -1,13 +1,17 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        d = defaultdict(int)
-        for i in range(len(s)):
-            l = []
-            if i+10 <= len(s):
-                l.append(s[i:i+10])
-            d["".join(l)] += 1
-        res = []
-        for key, value in d.items():
-            if key and value > 1:
-                res.append(key)
-        return res
+        res = set()
+        hashtable = defaultdict(int)
+        for i in range(9, len(s)):
+            hashtable[s[i-9:i+1]] += 1
+        for key,value in hashtable.items():
+            if value >= 2:
+                res.add(key)
+        return list(res)
+    
+    # Time Complexity: (N-10)10 -> slicing:10; for loop: N-10
+    # Space Complexity: (N-10)10
+    
+if __name__ == '__main__':
+    s = Solution()
+    #assert ["AAAAACCCCC","CCCCCAAAAA"] == sorted(s.findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"))
