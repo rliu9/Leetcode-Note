e14998500daaf454c029406775c708bb82bb77c3
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-07-04 13:22:15 -0400

    Time: 58 ms (38.41%), Space: 13.9 MB (32.00%) - LeetHub

--- 86-partition-list/86-partition-list.py
+++ 86-partition-list/86-partition-list.py
@@ -5,16 +5,19 @@
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        h1 = l1 = ListNode(0)
-        h2 = l2 = ListNode(0)
+        left = left_dummy = ListNode()
+        right = right_dummy = ListNode()
         while head:
             if head.val < x:
-                l1.next = head
-                l1 = l1.next
+                left.next = head
+                left = left.next
             else:
-                l2.next = head
-                l2 = l2.next
+                right.next = head
+                right = right.next
             head = head.next
-        l2.next = None
-        l1.next = h2.next
-        return h1.next
+        right.next = None
+        left.next = right_dummy.next
+        return left_dummy.next
+    
+    # Time Complexity: O(n)
+    # Space Complexity: O(1) -> two dummy nodes and no extra space
