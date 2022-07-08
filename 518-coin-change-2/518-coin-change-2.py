10cfcca2657d4fff68233d38356750aac5953b70
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-07-08 17:20:39 -0400

    Time: 284 ms (65.47%), Space: 14.1 MB (72.44%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -3,7 +3,7 @@ class Solution:
         dp = [0]*(amount+1)
         dp[0] = 1
         for coin in coins:
-            for i in range(coin, amount+1): #why from coin? -> to avoid duplicates
+            for i in range(coin, amount+1):
                 dp[i] += dp[i-coin]
         return dp[-1]
     
