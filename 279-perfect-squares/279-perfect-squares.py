b0610bbf82b961211d4546df6bc6e2ad1b090b13
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-07-08 12:18:01 -0400

    Time: 62 ms (97.07%), Space: 13.9 MB (70.58%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.py
+++ 279-perfect-squares/279-perfect-squares.py
@@ -1,6 +1,20 @@
 class Solution:
+    def isSquare(self, n: int) -> bool:
+        sq = int(math.sqrt(n))
+        return sq*sq == n
+
     def numSquares(self, n: int) -> int:
-        dp = [0]
-        while len(dp) <= n:
-            dp += min(dp[-i*i] for i in range(1, int(len(dp)**0.5+1))) + 1,
-        return dp[n]
+        # four-square and three-square theorems
+        while (n & 3) == 0:
+            n >>= 2      # reducing the 4^k factor from number
+        if (n & 7) == 7: # mod 8
+            return 4
+
+        if self.isSquare(n):
+            return 1
+        # check if the number can be decomposed into sum of two squares
+        for i in range(1, int(n**(0.5)) + 1):
+            if self.isSquare(n - i*i):
+                return 2
+        # bottom case from the three-square theorem
+        return 3
