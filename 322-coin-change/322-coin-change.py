0e5bbf658bb04c4c7be947ab8a46ea96439258ac
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-07-08 17:58:05 -0400

    Time: 1938 ms (61.47%), Space: 35.1 MB (11.77%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,5 +1,22 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        INF = math.inf
+        @lru_cache(None)
+        def dp(n):
+            if n < 0:
+                return -1
+            if n == 0:
+                return 0
+            ans = INF
+            for m in coins:
+                if dp(n-m) >= 0:
+                    ans = min(ans, 1 + dp(n-m))
+            return ans
+        return dp(amount) if dp(amount) < INF else -1
+        
+        
+        """
+        #bottom up
         dp = [float('inf')] * (amount+1)
         dp[0] = 0
         for coin in coins:
@@ -10,3 +27,5 @@ class Solution:
     
     # Time complexity: O(n*amount)
     # Space complexity: O(amount)
+        """
+        
