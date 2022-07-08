a409b90e7d722f23357dac5af1ed213cadd3c77b
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-07-08 12:12:48 -0400

    Time: 2806 ms (58.89%), Space: 13.8 MB (99.57%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.py
+++ 279-perfect-squares/279-perfect-squares.py
@@ -1,7 +1,6 @@
 class Solution:
-    _dp = [0]
     def numSquares(self, n: int) -> int:
-        dp = self._dp
+        dp = [0]
         while len(dp) <= n:
             dp += min(dp[-i*i] for i in range(1, int(len(dp)**0.5+1))) + 1,
         return dp[n]
