0d21628be05f62554b904b656790809777903f78
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-07-10 18:37:27 -0400

    Time: 47 ms (50.57%), Space: 14.2 MB (19.14%) - LeetHub

---
+++ 293-flip-game/293-flip-game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def generatePossibleNextMoves(self, currentState: str) -> List[str]:
+        res = []
+        for i in range(1, len(currentState)):
+            if currentState[i-1] == currentState[i] == '+':
+                res.append(currentState[:i-1]+'--'+currentState[i+1:])
+        return res
