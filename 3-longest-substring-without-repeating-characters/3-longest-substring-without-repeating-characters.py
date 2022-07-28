8846752287835e1c50fb63cc931a40dfcaf3614d
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-07-28 16:10:33 -0400

    Time: 73 ms (83.04%), Space: 14 MB (92.90%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,26 +1,11 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        i,res = 0,0
-        d = collections.defaultdict(int)
+        max_len, i = 0, 0
+        d = {}
         for j in range(len(s)):
             if s[j] in d:
                 i = max(i, d[s[j]]+1)
-            res = max(res, j-i+1)
+            max_len = max(max_len, j-i+1)
             d[s[j]] = j
-        return res
-    
-    # time complexity: O(n)
-    # space complexity: O(n)
-    
-if __name__ == '__main__':
-    """
-    abcabcbb
-    i = 0; j = 3 -> res = 3 -> string: abc
-    i = 1; j = 4 -> string: bca
-    i = 2; j = 5 -> string: cab
-    i = 3; j = 6 -> string: abc
-    i = 5; j = 7 -> string: cb
-    """
-    assert 3 == Solution().lengthOfLongestSubstring("abcabcbb")
-    assert 1 == Solution().lengthOfLongestSubstring("bbbbb")
-    assert 3 == Solution().lengthOfLongestSubstring("pwwkew")
+        return max_len
+            
