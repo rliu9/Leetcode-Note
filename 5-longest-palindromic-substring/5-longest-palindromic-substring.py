e7d508c03b8b11a1cfc712caf3b86f7ff98f3751
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-07-30 15:39:17 -0400

    Time: 654 ms (91.84%), Space: 13.9 MB (91.29%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,26 +1,16 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        # Form a bottom-up dp 2d array
-        # dp[i][j] will be 'true' if the string from index i to j is a palindrome. 
-        dp = [[False] * n  for _ in range(n)]
-        
-        ans = ''
-        # every string with one character is a palindrome
-        for i in range(n):
-            dp[i][i] = True
-            ans = s[i]
-            
-        maxLen = 1
-        for start in range(n-1, -1, -1):
-            for end in range(start+1, n):             
-				# palindrome condition
-                if s[start] == s[end]:
-                    # if it's a two char. string or if the remaining string is a palindrome too
-                    if end - start == 1 or dp[start+1][end-1]:
-                        dp[start][end] = True
-                        if maxLen < end - start + 1:
-                            maxLen = end - start + 1
-                            ans = s[start: end+ 1]
-        
-        return ans
+        res = ""
+        for i in range(len(s)):
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):res = tmp
+            tmp = self.helper(s, i, i+1)
+            if len(tmp) > len(res):res = tmp
+        return res
+
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1:r]
+    
+    
