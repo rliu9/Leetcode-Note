1ee4cee5dfb04222260c2ec0f4908efbc74afec8
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-08-01 16:58:57 -0400

    Time: 28 ms (99.44%), Space: 13.9 MB (79.66%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -1,23 +1,8 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        sign = 1 
-        result = 0
-        index = 0
-        n = len(s)
-        INT_MAX = pow(2,31) - 1 
-        INT_MIN = -pow(2,31)
-        while index < n and s[index] == ' ':
-            index += 1
-        if index < n and s[index] == '+':
-            sign = 1
-            index += 1
-        elif index < n and s[index] == '-':
-            sign = -1
-            index += 1
-        while index < n and s[index].isdigit():
-            digit = int(s[index]) 
-            if ((result > INT_MAX // 10) or (result == INT_MAX // 10 and digit > INT_MAX % 10)):
-                return INT_MAX if sign == 1 else INT_MIN
-            result = 10 * result + digit
-            index += 1
-        return result*sign
+        regex = r"\s*([+-]?)(\d+)"
+        match = re.match(regex, s)
+        value = 0
+        if match:
+            value = (min(2**31-1, int(match.group(2))) if match.group(1) != '-' else max(-1*int(match.group(2)),-2**31))
+        return value
