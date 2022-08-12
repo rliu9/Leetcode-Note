277c78e1de02b506f8d99016ceb7125444434ee7
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-08-11 22:03:12 -0400

    Time: 79 ms (55.32%), Space: 14.8 MB (32.64%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -1,15 +1,14 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
-        combined_info={}
+        combined_info=collections.defaultdict(list)
         for item in sorted(zip(username, timestamp, website), key = lambda k:(k[0],k[1])):
-            if item[0] in combined_info:
-                combined_info[item[0]].append(item[2])
-            else:
-                combined_info[item[0]]=[item[2]]
+            combined_info[item[0]].append(item[2])
+            
         patern_dict = {}
         for key in combined_info.keys():
             patern = set(combinations(combined_info[key],3))
             if patern:
                 patern_dict[key]=patern
         paternScore_dict = dict(Counter([d2 for d in patern_dict.values() for d2 in d]))
+        
         return sorted(paternScore_dict, key = lambda k:(-paternScore_dict[k],k))[0]
