bba66f69b7f89bf486a7f2f835187a69a7620093
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-08-11 22:16:55 -0400

    Time: 100 ms (25.61%), Space: 14.7 MB (54.67%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -3,12 +3,10 @@ class Solution:
         combined_info=collections.defaultdict(list)
         for item in sorted(zip(username, timestamp, website), key = lambda k:(k[0],k[1])):
             combined_info[item[0]].append(item[2])
-            
         patern_dict = {}
         for key in combined_info.keys():
             patern = set(combinations(combined_info[key],3))
             if patern:
                 patern_dict[key]=patern
         paternScore_dict = dict(Counter([d2 for d in patern_dict.values() for d2 in d]))
-        
-        return sorted(paternScore_dict, key = lambda k:(-paternScore_dict[k],k))[0]
+        return max(sorted(paternScore_dict), key=paternScore_dict.get)
