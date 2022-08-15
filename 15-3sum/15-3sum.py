ef5d4bdb6dceb37f9b691626ba2879fd45ba85dd
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-08-15 11:20:29 -0400

    Time: 3893 ms (19.86%), Space: 17.8 MB (84.29%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,12 +1,13 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        hashmap = {}
+        res = set()
         nums.sort()
-        res, s = set(), set()
-        for i in range(len(nums)):
+        for i,n in enumerate(nums):
             for j in range(i+1, len(nums)):
-                if -(nums[i]+nums[j]) in s:
-                    res.add((nums[i], nums[j], -(nums[i]+nums[j])))
-            s.add(nums[i])
+                if -nums[i]-nums[j] in hashmap:
+                    res.add((-nums[i]-nums[j], nums[i], nums[j]))
+            hashmap[n] = i
         return list(res)
     
     # time complexity: O(n^2)
