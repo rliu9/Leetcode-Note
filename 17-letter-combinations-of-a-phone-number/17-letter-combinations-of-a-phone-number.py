aa10536537408afb42824219c804dc028df10c51
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-08-16 11:23:41 -0400

    Time: 38 ms (79.06%), Space: 14 MB (32.00%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,27 +1,23 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        numbers = {
-            '2':'abc',
-            '3':'def',
-            '4':'ghi',
-            '5':'jkl',
-            '6':'mno',
-            '7':'pqrs',
-            '8':'tuv',
-            '9':'wxyz'
+        phone = {
+            2: 'abc',
+            3: 'def',
+            4: 'ghi',
+            5: 'jkl',
+            6: 'mno',
+            7: 'pqrs',
+            8: 'tuv',
+            9: 'wxyz'
         }
         res = []
-        for i in digits:
-            res.append(numbers[i])
+        for i in digits:res.append(phone[int(i)])
         while len(res) > 1:
             l1 = res.pop()
             l2 = res.pop()
-            add = []
+            temp = []
             for i in l2:
                 for j in l1:
-                    add.append(i+j)
-            res.append(add)
+                    temp.append(i+j)
+            res.append(temp)
         return res[0] if res else []
-    
-    # O(N*4^N)
-    # O(N)
