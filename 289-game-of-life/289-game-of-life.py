295a34ca440c3b36ff3bc876ea70772f244d09e1
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-08-21 13:32:36 -0400

    Time: 70 ms (10.09%), Space: 14 MB (48.70%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -3,7 +3,6 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        l1, l2 = [], []
         row, col = len(board), len(board[0])
         neighbors = [(0,-1),(0,1),(-1,-1),(-1,0),(-1,1),(1,-1),(1,0),(1,1)]
         for r in range(row):
@@ -11,15 +10,20 @@ class Solution:
                 live = 0
                 for x, y in neighbors:
                     rr, cc = r+x, c+y
-                    if rr<row and rr>=0 and cc<col and cc>=0 and board[rr][cc] == 1:
+                    if rr<row and rr>=0 and cc<col and cc>=0 and abs(board[rr][cc]) == 1:
                         live += 1
-                if board[r][c] == 1 and (live < 2 or live > 3):l1.append((r,c))
-                if board[r][c] == 0 and live == 3:l2.append((r,c))
+                if board[r][c] == 1:
+                    if live < 2 or live > 3:board[r][c] = -1
+                else:
+                    if live == 3:board[r][c] = 2
         
-        for r,c in l1:board[r][c] = 0
-        for r,c in l2:board[r][c] = 1
+        for r in range(row):
+            for c in range(col):
+                if board[r][c] == 2:board[r][c] = 1
+                elif board[r][c] == -1:board[r][c] = 0
         
-                        
+        # O(M*N)
+        # O(1)
                         
                             
                     
