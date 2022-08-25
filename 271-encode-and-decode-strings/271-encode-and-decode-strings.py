075f18c1bc14d7411aec3996ceb21646ebfa484c
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-08-25 18:17:45 -0400

    Time: 89 ms (72.34%), Space: 14.3 MB (52.85%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.py
@@ -3,24 +3,16 @@ class Codec:
         """Encodes a list of strings to a single string.
         """
         buffer = []
-        for s in strs:
-            buffer += [chr(len(s)), s]
+        for i, s in enumerate(strs):
+            if i == 0:buffer+=[s]
+            else:buffer += ['.hardtoguest.', s]
         return ''.join(buffer)
         
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        res = []
-        pos = 0
-        while pos < len(s):
-            str_len = ord(s[pos])
-            pos += 1
-            if pos + str_len > len(s):
-                return
-            res.append(s[pos:pos + str_len])
-            pos += str_len
-        return res
+        return s.split('.hardtoguest.')
 
 
 # Your Codec object will be instantiated and called as such:
