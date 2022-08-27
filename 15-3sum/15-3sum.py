2fb247d68bd996370fe5f78215ef45fac0841295
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-08-27 11:52:19 -0400

    Time: 807 ms (88.02%), Space: 17.2 MB (91.54%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -11,19 +11,21 @@ class Solution:
         return list(res)"""
         res = []
         nums.sort()
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i - 1]:
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            l, r = i + 1, len(nums) - 1
+            l, r = i+1, len(nums)-1
             while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:r -= 1
-                elif threeSum < 0:l += 1
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:l +=1 
+                elif s > 0:r -= 1
                 else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l - 1] and l < r:
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l+1]:
                         l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1; r -= 1
         return res
     # time complexity: O(n^2)
     # space complexity: O(n)
