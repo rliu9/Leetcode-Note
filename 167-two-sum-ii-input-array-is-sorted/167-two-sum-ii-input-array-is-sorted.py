d7c90711629c841d2ddbd6ed3ab7a7e0d86789de
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-08-27 10:20:58 -0400

    Time: 146 ms (83.18%), Space: 14.9 MB (42.04%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,18 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        d = {}
-        for i, n in enumerate(numbers):
-            if target - n in d:
-                return [d[target-n]+1, i+1]
-            d[n] = i
+        l, r = 0, len(numbers)-1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:return[l+1,r+1]
+            if s < target:l += 1
+            else:r -= 1
         return -1
+    
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    
+if __name__ == '__main__':
+    assert Solution().twoSum([2,7,11,15], 9) == [1,2]
+    assert Solution().twoSum([2,3,4], 6) == [1,3]
+    assert Solution().twoSum([-1,0], -1) == [1,2]
+                
