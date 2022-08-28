652f50bf244b349e2720b9e09c08772da2bb0018
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-08-28 11:14:39 -0400

    Time: 443 ms (63.07%), Space: 16.5 MB (65.90%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,17 +1,16 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        def bfs(i, j, visited):
-            visited.add((i,j))
+        def bfs(i, j):
+            grid[i][j] = "0"
             for x, y in (1, 0), (0, 1), (-1, 0), (0, -1):
                 ix, jy = x+i, y+j
-                if not (0 <= ix < rows) or not (0 <= jy < cols) or (ix, jy) in visited or grid[ix][jy] != "1":continue
-                bfs(ix, jy, visited)
-        res, visited, q = 0, set(), deque([])
+                if not (0 <= ix < rows) or not (0 <= jy < cols) or grid[ix][jy] != "1":continue
+                bfs(ix, jy)
+        res = 0
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    bfs(r, c, visited)
+                if grid[r][c] == "1":
+                    bfs(r, c)
                     res += 1
-
         return res
