dd5723f44fa0af88ac656095b8d4eda8516a4753
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-09-01 10:17:12 -0400

    Time: 414 ms (62.45%), Space: 14.6 MB (70.43%) - LeetHub

--- 490-the-maze/490-the-maze.py
+++ 490-the-maze/490-the-maze.py
@@ -5,10 +5,10 @@ class Solution:
         visit = set()
         while q:
             x, y = q.popleft()
-            if x == destination[0] and y == destination[1]:return True
+            if [x,y] == destination:return True
             for dx, dy in (0,1), (1,0), (0,-1), (-1,0):
                 i, j = x+dx, y+dy
-                while 0<=i<rows and 0<=j<cols and not maze[i][j]:
+                while 0<=i<rows and 0<=j<cols and maze[i][j] != 1:
                     i, j = i+dx, j+dy
                 i, j = i-dx, j-dy
                 if (i,j) not in visit:
