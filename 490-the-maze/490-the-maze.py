ae385df6cc50e27243b2f568e0088b51baf07e8c
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-09-01 09:26:46 -0400

    Time: 624 ms (20.44%), Space: 14.5 MB (70.43%) - LeetHub

--- 490-the-maze/490-the-maze.py
+++ 490-the-maze/490-the-maze.py
@@ -1,17 +1,17 @@
 class Solution:
     def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
         q = collections.deque([start])
-        visited = set()
+        rows, cols = len(maze), len(maze[0])
+        visit = set()
         while q:
-            i,j = q.popleft()
-            if i == destination[0] and j == destination[1]:
-                return True
-            for di,dj in [[-1,0],[0,-1],[1,0],[0,1]]:
-                ni, nj = i + di  , j + dj
-                while 0 <= ni < len(maze) and 0 <= nj < len(maze[0]) and not maze[ni][nj] :
-                    ni, nj  = ni + di , nj + dj
-                ni, nj  = ni - di, nj - dj
-                if (ni,nj) not in visited:
-                    visited.add((ni,nj))
-                    q.append((ni,nj))
+            x, y = q.popleft()
+            if x == destination[0] and y == destination[1]:return True
+            for dx, dy in (0,1), (1,0), (0,-1), (-1,0):
+                i, j = x+dx, y+dy
+                while 0<=i<rows and 0<=j<cols and not maze[i][j]:
+                    i, j = i+dx, j+dy
+                i, j = i-dx, j-dy
+                if (i,j) not in visit:
+                    visit.add((i,j))
+                    q.append((i,j))
         return False
