9005d4d8bda11d1002d6b75225dc8f0f8304c0d1
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-09-11 12:10:48 -0400

    Time: 179 ms (42.86%), Space: 14.6 MB (100.00%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        cur = ''
+        res = 1
+        for i in s:
+            if i in cur:
+                res += 1
+                cur = i
+            else:
+                cur += i
+        return res
