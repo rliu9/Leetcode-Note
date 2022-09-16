d17ce54f5449986c8376796942a5cf2f1b1cd031
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-09-16 11:21:26 -0400

    Time: 100 ms (5.21%), Space: 14.1 MB (22.83%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,3 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(itertools.permutations(nums, len(nums)))
+        res = []
+        def backtracking(cur, counter):
+            if len(cur) == len(nums):
+                res.append(cur[:])
+            for c in counter:
+                if counter[c] > 0:
+                    cur.append(c)
+                    counter[c] -= 1
+                    backtracking(cur, counter)
+                    counter[c] += 1
+                    cur.pop()
+        backtracking([], collections.Counter(nums))
+        return res
