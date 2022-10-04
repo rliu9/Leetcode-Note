eda90a2e3a96c0ef303c52527b17aaae44de1797
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-10-03 23:19:42 -0400

    Time: 4865 ms (11.46%), Space: 14.1 MB (30.93%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,6 +1,12 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        counter, n = collections.Counter(s1), len(s1)
+        counter, k = collections.Counter(s1), len(s1)
         for i in range(len(s2)):
-            if counter == collections.Counter(s2[i:i+n]):return True
+            if counter == collections.Counter(s2[i:i+k]):return True
         return False
+    
+    # O(n k^2)
+    # O(1) because counter only contains at most 26 key-value pairs
+    
+    
+    
