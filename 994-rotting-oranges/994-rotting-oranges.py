3a508c1ef4c3ab6bf6d0a406f47539beb6fcea35
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-11-17 18:05:33 -0500

    Time: 134 ms (14.33%), Space: 14 MB (49.28%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,26 +1,25 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        row, col, q, fresh, seen = len(grid), len(grid[0]), deque([]), 0, set();
+        row, col, seen, q, fresh, count = len(grid), len(grid[0]), set(), deque([]), 0, 0
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == 2:
                     q.append((r,c))
+                    seen.add((r,c))
                 elif grid[r][c] == 1:
                     fresh += 1
-        seen.update(q)
-        res = 0
         while q:
             for _ in range(len(q)):
                 x, y = q.popleft()
-                for i, j in (x+1,y), (x-1,y), (x,y+1), (x,y-1) :
-                    if 0 <= i < row and 0 <= j < col and (i,j) not in seen and grid[i][j] != 0:
-                        grid[i][j] = 1
+                grid[x][y] = 2
+                for r, c in (x+1, y), (x-1, y), (x, y+1), (x, y-1):
+                    if 0 <= r < row and 0 <= c < col and (r,c) not in seen and grid[r][c] == 1:
+                        q.append((r,c))
+                        seen.add((r,c))
                         fresh -= 1
-                        seen.add((i,j))
-                        q.append((i,j))
-            res += 1
-        if res > 0:res-=1
-        return res if fresh == 0 else -1
+            count += 1
+        if count > 0: count -= 1
+        return count if fresh == 0 else -1
     
 if __name__ == '__main__':
     solution = Solution()
