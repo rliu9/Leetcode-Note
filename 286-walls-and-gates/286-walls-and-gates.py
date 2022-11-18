ca2796e5981e1d51112352156af350b33dba7226
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-11-17 20:47:12 -0500

    Time: 307 ms (87.44%), Space: 20.5 MB (34.07%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -3,35 +3,24 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        ROWS, COLS = len(rooms), len(rooms[0])
-        visit = set()
-        q = deque()
-
-        def addRooms(r, c):
-            if (
-                min(r, c) < 0
-                or r == ROWS
-                or c == COLS
-                or (r, c) in visit
-                or rooms[r][c] == -1
-            ):
-                return
-            visit.add((r, c))
-            q.append([r, c])
-
-        for r in range(ROWS):
-            for c in range(COLS):
+        row, col = len(rooms), len(rooms[0])
+        q, seen = deque([]), set()
+        def addRoom(r,c):
+            if 0 <= r < row and 0 <= c < col and (r,c) not in seen and rooms[r][c] != -1:
+                q.append((r,c))
+                seen.add((r,c))
+        for r in range(row):
+            for c in range(col):
                 if rooms[r][c] == 0:
-                    q.append([r, c])
-                    visit.add((r, c))
-
+                    q.append((r,c))
+                    seen.add((r,c))
         dist = 0
         while q:
-            for i in range(len(q)):
-                r, c = q.popleft()
-                rooms[r][c] = dist
-                addRooms(r + 1, c)
-                addRooms(r - 1, c)
-                addRooms(r, c + 1)
-                addRooms(r, c - 1)
+            for _ in range(len(q)):
+                x, y = q.popleft()
+                rooms[x][y] = dist
+                addRoom(x+1, y)
+                addRoom(x-1, y)
+                addRoom(x, y+1)
+                addRoom(x, y-1)
             dist += 1
