18304047a1d3d4cb3f84825a2cc8fe8402874524
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-11-29 15:50:27 -0500

    Time: 74 ms (11.33%), Space: 14.2 MB (7.08%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -1,23 +1,16 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        matrix = [[0]*n for _ in range(n)]
-        r,rr,c,cc = 0,n,0,n
-        value = 1
-        while value <= n**2:
-            for i in range(c, cc):
-                matrix[r][i] = value
-                value += 1
-            r += 1
-            for i in range(r, rr):
-                matrix[i][cc-1] = value
-                value += 1
-            cc -= 1
-            for i in range(cc-1, c-1, -1):
-                matrix[rr-1][i] = value
-                value += 1
-            rr -= 1
-            for i in range(rr-1, r-1, -1):
-                matrix[i][c] = value
-                value += 1
-            c += 1
-        return matrix
+        res = [[0]*n for _ in range(n)]
+        self.i = 1
+        def dfs(r, c, going_up):
+            if 0 <= r < n and 0 <= c < n and res[r][c] == 0:
+                res[r][c] = self.i
+                self.i += 1
+                if going_up:
+                    dfs(r-1, c, True)
+                dfs(r, c+1, False)
+                dfs(r+1, c, False)
+                dfs(r, c-1, False)
+                dfs(r-1, c, True)
+        dfs(0, 0, False)
+        return res
