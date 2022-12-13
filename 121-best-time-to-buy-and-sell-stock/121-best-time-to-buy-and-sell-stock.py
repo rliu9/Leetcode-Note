d8950fb2fdd780799d05c5b912a8854b27d608d5
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-12-13 14:05:49 -0500

    Time: 1092 ms (90.99%), Space: 25 MB (87.27%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res = 0
-        _min = prices[0]
-        for i in range(1, len(prices)):
-            _min = min(_min, prices[i])
-            res = max(res, prices[i] - _min)
+        # sliding windows
+        res, left = 0, 0
+        for right in range(1, len(prices)):
+            if prices[left] > prices[right]:
+                left = right
+            res = max(res, prices[right]-prices[left])
         return res
+            
