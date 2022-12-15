f5e91500e7b0648e27459c05babf5c140f4423d2
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-12-15 17:43:29 -0500

    Time: 87 ms (88.82%), Space: 17.5 MB (25.38%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -3,9 +3,16 @@ class Solution:
         intervals.append(newInterval)
         intervals.sort(key=lambda x:x[0])
         res = []
-        for i in intervals:
-            if not res or i[0] > res[-1][-1]:
-                res.append(i)
+        for interval in intervals:
+            if not res or interval[0] > res[-1][-1]:
+                res.append(interval)
             else:
-                res[-1][-1] = max(res[-1][-1], i[1])
+                res[-1][-1] = max(res[-1][-1], interval[1])
         return res
+    
+    # O(nlogn)
+    # O(n)
+if __name__ == '__main__':
+    s = Solution()
+    assert s.insert([[1,3],[6,9]], [2,5]) == [[1,5],[6,9]]
+    assert s.insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]) == [[1,2],[3,10],[12,16]]
