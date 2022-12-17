0952fb82378c1736648fb64f286739f7693e9e7e
Rundi Liu <44102063+freakleesin@users.noreply.github.com>
2022-12-17 00:06:12 -0500

    Time: 92 ms (81.48%), Space: 17.4 MB (84.77%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         heap = []
-        intervals.sort(key=lambda x: x[0])
-        for x, y in intervals:
-            if not heap or heap[0] > x:
+        for start,end in sorted(intervals):
+            if not heap or heap[0] > start:
                 # rooms are still in use
-                heapq.heappush(heap, y)
-            else: # free room available , replace with current meeting 
-                heapq.heappushpop(heap,y)
+                heapq.heappush(heap, end)
+            else: # free room available , replace with current meeting
+                heapq.heappop(heap)
+                heapq.heappush(heap,end)
         return len(heap)
     
     # O(nlogn)
