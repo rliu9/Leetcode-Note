481c787b77f5bfeb54a5444a914fbab65fba8f05
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2022-12-19 13:30:36 -0500

    Time: 850 ms (78.83%), Space: 71.2 MB (76.14%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -8,17 +8,14 @@ class TimeMap:
 
     def get(self, key: str, timestamp: int) -> str:
         if key in self.d:
-            li = self.d[key]
-            n = len(li)
+            li, n = self.d[key], len(self.d[key])
             l, r = 0, n-1
             if li[l][0] > timestamp:return ''
             while l <= r:
-                mid = (l+r) // 2
+                mid = l + (r-l) // 2
                 if li[mid][0] == timestamp:return li[mid][1]
-                if li[mid][0] <= timestamp:
-                    l = mid + 1
-                else:
-                    r = mid - 1
+                if li[mid][0] < timestamp:l = mid + 1
+                else:r = mid - 1
             return li[r][1]
         return ''
 
