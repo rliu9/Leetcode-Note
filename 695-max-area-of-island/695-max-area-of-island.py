ddf3130daec13e66bb3f77c3d863616f6d2eabb7
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2022-12-20 12:19:57 -0500

    Time: 161 ms (80.22%), Space: 14.1 MB (92.43%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,14 +1,21 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         row, col = len(grid), len(grid[0])
-        def dfs(r, c):
-            if 0 <= r < row and 0 <= c < col and grid[r][c] == 1:
-                grid[r][c] = 0
-                return 1 + dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1)
-            return 0
+        def bfs(q):
+            ret = 0
+            while q:
+                r, c = q.popleft()
+                if 0 <= r < row and 0 <= c < col and grid[r][c] == 1:
+                    grid[r][c] = 0
+                    ret += 1
+                    q.append((r+1,c))
+                    q.append((r-1,c))
+                    q.append((r,c+1))
+                    q.append((r,c-1))
+            return ret
         res = 0
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == 1:
-                    res = max(res, dfs(r,c))
+                    res = max(res, bfs(deque([(r,c)])))
         return res
