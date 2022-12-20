d1dc971dacc044e0a31cca037cfb26efcb084810
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2022-12-20 11:04:21 -0500

    Time: 515 ms (66.65%), Space: 16.4 MB (82.66%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,22 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        def bfs(q):
-            while q:
-                i, j = q.popleft()
-                for x, y in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                    ix, jy = x+i, y+j
-                    if not (0<=ix<rows) or not (0<=jy<cols) or grid[ix][jy] == "0":continue
-                    q.append((ix, jy))
-                    grid[ix][jy] = "0"
-                
+        row, col = len(grid), len(grid[0])
+        def dfs(r, c):
+            if 0 <= r < row and 0 <= c < col and grid[r][c] == '1':
+                grid[r][c] = '0'
+                dfs(r+1, c)
+                dfs(r-1, c)
+                dfs(r, c+1)
+                dfs(r, c-1)
+        
         res = 0
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1":
-                    bfs(deque([(r,c)]))
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == '1':
+                    dfs(r,c)
                     res += 1
         return res
-    
-    # O (M*N)
-    # O (min(M,N))
