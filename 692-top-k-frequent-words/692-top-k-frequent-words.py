0b81028a29a6f72528f3172701be11987141cb7d
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2022-12-30 14:38:21 -0500

    Time: 65 ms (75.41%), Space: 14 MB (27.07%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,18 +1,4 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        c = collections.Counter(words)
-        d = dict(sorted(c.items(), key=lambda x:x[1]))
-        dd = collections.defaultdict(list)
-        for key,value in d.items():
-            dd[value].append(key)
-        
-        dd = dict(sorted(dd.items(), key=lambda x:-x[0]))
-        res = []
-        for i in range(k):
-            for k in dd:
-                value = min(dd[k])
-                res.append(value)
-                dd[k].remove(value)
-                if not dd[k]:del dd[k]
-                break
-        return res
+        cnt = Counter(words)
+        return nsmallest(k, cnt.keys(), key=lambda x: (-cnt[x], x))
