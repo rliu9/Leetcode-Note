92b7343131dd2d3c8d60a7c376878e41c8845b97
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2022-12-31 22:39:17 -0500

    Time: 36 ms (80.94%), Space: 14 MB (23.57%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        d = {}
+        def dfs(root, depth):
+            if not root:return
+            d[depth] = root.val
+            dfs(root.left, depth+1)
+            dfs(root.right, depth+1)
+        dfs(root, 0)
+        return d.values()
