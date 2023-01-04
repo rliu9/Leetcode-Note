1e9ec134bd67985f71c669c2213255df6ce83b68
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-01-04 14:28:00 -0500

    Time: 68 ms (13.92%), Space: 13.9 MB (48.95%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,3 +1,12 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        return("{0:b}".format(n).count('1'))
+        #return("{0:b}".format(n).count('1'))
+        
+        # n & (n-1) will remove rightmost 1
+        # n:1010100    n-1:1010011    n&(n-1):1010000
+        res = 0
+        while n > 0:
+            n = n & (n-1)
+            res += 1
+        return res
+        
