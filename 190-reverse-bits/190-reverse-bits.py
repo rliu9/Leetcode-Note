36f196afab633846b6fc2fb7fe3b2f8f9e3f2e35
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-01-04 16:06:39 -0500

    Time: 60 ms (38.36%), Space: 13.9 MB (7.05%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -1,3 +1,12 @@
 class Solution:
     def reverseBits(self, n: int) -> int:        
-        return int('{0:032b}'.format(n)[::-1], 2)
+        # return int('{0:032b}'.format(n)[::-1], 2)
+        res = 0
+        for i in range(32):
+            if n & 1:
+                res += 1 << (31-i)
+            n >>= 1
+        return res
+    
+    # O(1)
+    # O(1)
