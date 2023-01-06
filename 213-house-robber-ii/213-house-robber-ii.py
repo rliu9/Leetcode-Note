abb4ecb70d9095dd19439f14c06f606f7bf6b702
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-01-06 17:10:23 -0500

    Time: 32 ms (88.89%), Space: 13.9 MB (70.23%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -7,3 +7,11 @@ class Solution:
                 dp += [max(dp[i-1], dp[i-2]+nums[i])]
             return max(dp[-1],dp[-2])
         return max(nums) if len(nums)<=2 else max(getmax(nums[1:]), getmax(nums[:-1]))
+    
+    # O(n)
+    # O(n)
+    
+if __name__ == '__main__':
+    s = Solution()
+    assert s.rob([1,2,3,1]) == 4
+    assert s.rob([2,3,2]) == 3
