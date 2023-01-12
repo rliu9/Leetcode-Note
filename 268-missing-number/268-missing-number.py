773cfde75d87cab781ce33d45fcf754fd838ef60
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-01-12 14:48:01 -0500

    Time: 123 ms (99.24%), Space: 15.2 MB (76.58%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,4 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        
         n = len(nums)
-        return ((n * (n+1)) // 2) - sum(nums) 
+        return ((n * (n+1)) // 2) - sum(nums)
+        # O(n)
+        # O(1)
