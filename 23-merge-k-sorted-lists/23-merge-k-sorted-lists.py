2eca17d890566d87bc522c4489ca1a0d2252b4a4
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-01-13 14:19:00 -0500

    Time: 115 ms (73.71%), Space: 17.7 MB (58.63%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -6,12 +6,11 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         heap = []
-        cur = dummy_head = ListNode(0)
+        dummy_head = cur = ListNode()
         for i in range(len(lists)):
             if lists[i]:
                 heapq.heappush(heap, (lists[i].val, i))
                 lists[i] = lists[i].next
-        
         while heap:
             val, i = heapq.heappop(heap)
             cur.next = ListNode(val)
@@ -19,8 +18,7 @@ class Solution:
             if lists[i]:
                 heapq.heappush(heap, (lists[i].val, i))
                 lists[i] = lists[i].next
-            
         return dummy_head.next
     
     # Time Complexity: O(n logk) k is the number of linkedlists
-    # Space ComplexityL O(n)
+    # Space Complexity: O(n)
