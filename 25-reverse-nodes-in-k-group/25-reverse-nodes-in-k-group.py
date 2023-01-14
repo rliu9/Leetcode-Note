97fcb189da6d0f188df2ae162d5d674c5f630271
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-01-14 11:07:32 -0500

    Time: 46 ms (92.75%), Space: 15.3 MB (12.16%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
@@ -5,19 +5,20 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy_head = jump = ListNode(next=head)
+        dummy = jump = ListNode(next=head)
         left = right = head
         while True:
-            count = 0
-            while right and count < k:
+            i = 0
+            while right and i < k:
                 right = right.next
-                count += 1
-            if count == k:
+                i += 1
+            if i == k:
                 pre, cur = right, left
                 for _ in range(k):
-                    cur.next, cur, pre = pre, cur.next, cur
+                    cur.next, pre, cur = pre, cur, cur.next
                 jump.next, jump, left = pre, left, right
+                    
             else:
-                return dummy_head.next
+                return dummy.next
                 
             
