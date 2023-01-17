deba812477508ab30894edabd84c3d873095ec63
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-01-17 00:28:34 -0500

    Time: 58 ms (57.89%), Space: 14.6 MB (78.42%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -3,13 +3,11 @@ class Solution:
         left, right = 0, len(nums)-1
         while left <= right:
             mid = (left+right)//2
-            if nums[mid] == target:
-                return mid
-            if nums[mid] < target:
-                left = mid+1
-            else:
-                right = mid-1
+            if nums[mid] == target:return mid
+            if nums[mid] < target:left = mid+1
+            else:right = mid-1
         return left
+    
     
     # Time Complexity: O(logn)
     # Space Complexity: O(1)
