1ef28d8cfafb5669f52a86a1dc0bc139fa10655b
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-01-26 18:44:59 -0500

    Time: 1002 ms (12.37%), Space: 60.8 MB (9.08%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.py
@@ -3,5 +3,10 @@ class Solution:
         c = collections.Counter(s)
         for i, n in enumerate(s):
             if c[n] < k:
-                return max(self.longestSubstring(s[:i], k), self.longestSubstring(s[i+1:], k))
+                left = self.longestSubstring(s[:i], k)
+                right = self.longestSubstring(s[i+1:], k)
+                return max(left, right)
         return len(s)
+    
+    # O(n^2)
+    # O(n)
