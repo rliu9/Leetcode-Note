f3feecf7c3f19fefff6488f418582d93a6bedbee
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-02-06 14:25:28 -0500

    Time: 53 ms (65.87%), Space: 13.9 MB (29.31%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,28 +1,31 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        _dict = {
-            1: 'I',
-            2: 'II',
-            3: 'III',
-            4: 'IV',
-            5: 'V',
-            6: 'VI',
-            7: 'VII',
-            8: 'VIII',
-            9: 'IX',
-            10: 'X',
-            40: 'XL',
-            50: 'L',
-            90: 'XC',
-            100: 'C',
-            400: 'CD',
-            500: 'D',
-            900: 'CM',
-            1000: 'M'
+        d = {
+            1:"I",
+            2:"II",
+            3:"III",
+            4:"IV",
+            5:"V",
+            6:"VI",
+            7:"VII",
+            8:"VIII",
+            9:"IX",
+            10:"X",
+            40:"XL",
+            50:"L",
+            90:"XC",
+            100:"C",
+            400:"CD",
+            500:"D",
+            900:"CM",
+            1000:"M",
         }
         res = ''
-        for i in sorted(_dict.keys(), reverse=True):
+        for i in reversed(d):
             amount = num // i
-            res += _dict[i]*amount
-            num -= i*amount
+            res += amount*d[i]
+            num -= amount*i
         return res
+    
+    # O(1)
+    # O(1)
