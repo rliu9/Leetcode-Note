c23fdb2f373f7bd856042d652f9dd4007bd6f54e
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-02-08 11:10:29 -0500

    Time: 89 ms (95.65%), Space: 13.9 MB (27.40%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,18 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows, cols, boxes = [set() for _ in range(9)], [set() for _ in range(9)], [set() for _ in range(9)]
+        rows,cols,boxes = [set() for _ in range(9)],[set() for _ in range(9)],[set() for _ in range(9)]
         for r in range(9):
             for c in range(9):
-                val = board[r][c]
-                if val == '.':continue
-                if val in rows[r]:return False
-                rows[r].add(val)
-                if val in cols[c]:return False
-                cols[c].add(val)
-                idx = (r//3)*3 + c//3
-                if val in boxes[idx]:return False
-                boxes[idx].add(val)
+                if board[r][c] == '.':continue
+                if board[r][c] in rows[r]:return False
+                rows[r].add(board[r][c])
+                if board[r][c] in cols[c]:return False
+                cols[c].add(board[r][c])
+                i = (r//3)*3 + c//3
+                if board[r][c] in boxes[i]:return False
+                boxes[i].add(board[r][c])
         return True
-    
-    # Time Complexity: O(N^2)
-    # Space Complexity: O(N^2)
+        
+    # Time Complexity: O(9^2) = O(1)
+    # Space Complexity: O(9^2) = O(1)
