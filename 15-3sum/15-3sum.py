4ca8a99d240923b6e4c87445effaac1dce25f913
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-02-08 21:28:25 -0500

    Time: 3128 ms (25.10%), Space: 18.6 MB (15.59%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,30 +1,14 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        """visit = set()
-        res = set()
         nums.sort()
-        for i,n in enumerate(nums):
+        visit, res = set(), set()
+        for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if -nums[i]-nums[j] in visit:
                     res.add((-nums[i]-nums[j], nums[i], nums[j]))
-            visit.add(n)
-        return list(res)"""
-        res = []
-        nums.sort()
-        for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            l, r = i+1, len(nums)-1
-            while l < r:
-                s = nums[i] + nums[l] + nums[r]
-                if s < 0:l +=1 
-                elif s > 0:r -= 1
-                else:
-                    res.append((nums[i], nums[l], nums[r]))
-                    l += 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
-        return res
+            visit.add(nums[i])
+        return list(res)
+                
     # time complexity: O(n^2)
     # space complexity: O(n)
 
