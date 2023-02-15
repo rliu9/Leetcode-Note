42d50c6711bf4de3639fb0e886a8a344d26d35ce
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-02-15 00:32:06 -0500

    Time: 56 ms (57.42%), Space: 13.9 MB (39.70%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,27 +1,30 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        row, col, seen, q, fresh, count = len(grid), len(grid[0]), set(), deque([]), 0, 0
+        # bfs
+        q, row, col = deque([]), len(grid), len(grid[0])
+        fresh = 0
+        res = 0
+        visit = set()
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == 2:
                     q.append((r,c))
-                    seen.add((r,c))
-                elif grid[r][c] == 1:
-                    fresh += 1
+                    visit.add((r,c))
+                elif grid[r][c] == 1:fresh += 1
+                    
         while q:
             for _ in range(len(q)):
                 x, y = q.popleft()
                 grid[x][y] = 2
-                for r, c in (x+1, y), (x-1, y), (x, y+1), (x, y-1):
-                    if 0 <= r < row and 0 <= c < col and (r,c) not in seen and grid[r][c] == 1:
+                for r,c in (x+1, y), (x-1, y), (x, y+1), (x, y-1):
+                    if 0 <= r < row and 0 <= c < col and grid[r][c] == 1 and (r,c) not in visit:
                         q.append((r,c))
-                        seen.add((r,c))
                         fresh -= 1
-            count += 1
-        if count > 0: count -= 1
-        return count if fresh == 0 else -1
-    
-if __name__ == '__main__':
-    solution = Solution()
-    assert 4 == solution.orangesRotting([[2,1,1],[1,1,0],[0,1,1]])
-    assert -1 == solution.orangesRotting([[2,1,1],[0,1,1],[1,0,1]])
+                        visit.add((r,c))
+            res += 1
+        if res == 0:return res if fresh == 0 else -1
+        return res-1 if fresh == 0 else -1
+        
+        
+        
+        
