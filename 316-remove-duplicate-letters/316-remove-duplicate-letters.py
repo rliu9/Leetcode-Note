8d014edb8c206bcb4b6c1a561d068f7337bad414
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-02-18 19:34:49 -0500

    Time: 82 ms (10.07%), Space: 14.1 MB (13.19%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -2,9 +2,12 @@ class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         c = collections.Counter(s)
         idx = 0
-        for i,n in enumerate(s):
-            if n < s[idx]:
+        for i in range(len(s)):
+            if s[idx] > s[i]:
                 idx = i
-            c[n] -= 1
-            if c[n] == 0:break
+            c[s[i]] -= 1
+            if c[s[i]] == 0:break
         return s[idx] + self.removeDuplicateLetters(s[idx:].replace(s[idx], '')) if s else ''
+    
+    # O(n) recursive calls is bounded by 26 O(n) * 26
+    # O(n) we're creating a new string while slicing
