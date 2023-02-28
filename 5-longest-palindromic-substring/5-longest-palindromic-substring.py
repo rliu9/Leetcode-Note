ba003a5f6657f22aef7316aaf65d9bad7e5e36a6
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-02-28 14:21:32 -0500

    Time: 539 ms (79.29%), Space: 13.9 MB (56.00%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -2,13 +2,25 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         l, r = 0, 0
         for i in range(len(s)):
-            odd = self.helper(s, i, i)
-            even = self.helper(s, i, i+1)
+            odd = self.palindrome(s, i, i)
+            even = self.palindrome(s, i, i+1)
             l, r = max(odd, even, (l,r), key=lambda x:x[1]-x[0])
-        return s[l:r]
+        return s[l+1:r]
     
-    def helper(self, s, l, r):
+    def palindrome(self, s, l, r):
         while l >= 0 and r < len(s) and s[l] == s[r]:
             l -= 1
             r += 1
-        return l+1, r
+        return l, r
+    
+    # Time Complexity: O(n^2)
+    # Space Complexity: O(1)
+    
+    # Time Complexity: O(n^2)
+    # Space Complexity: O(1)
+    
+    
+    
+if __name__ == '__main__':
+    s = Solution()
+    assert s.longestPalindrome("babad") == "aba"
