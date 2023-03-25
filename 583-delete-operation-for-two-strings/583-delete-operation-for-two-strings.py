23885d086703ca000dadee1184380966e8235b41
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-03-25 11:21:49 -0400

    Time: 234 ms (93.47%), Space: 15.9 MB (79.15%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -1,15 +1,17 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         # LCS - longest common subsequence
-        dp = [[0]*(len(word2)+1) for _ in range(len(word1)+1)]
-        for j in range(len(word2)-1, -1, -1):
-            for i in range(len(word1)-1, -1, -1):
+        n1, n2 = len(word1), len(word2)
+        dp = [[0] * (n2+1) for _ in range(n1+1)]
+        for j in range(n2-1, -1, -1):
+            for i in range(n1-1, -1, -1):
                 if word2[j] == word1[i]:
                     dp[i][j] = dp[i+1][j+1] + 1
                 else:
                     dp[i][j] = max(dp[i+1][j], dp[i][j+1])
         longest_len = dp[0][0]
-        return len(word1) - longest_len + len(word2) - longest_len
+        return n1-longest_len+n2-longest_len
+
     
     # Time complexity: O(M*N)
     # Space complexity: O(M*N)
