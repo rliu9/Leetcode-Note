ff019aaf3a6786df48d9742094c83132407ca7c5
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-04-08 18:17:49 -0400

    Time: 542 ms (93.20%), Space: 28.8 MB (78.80%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,10 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashset = set()
-        for n in nums:
-            if n in hashset:return True
-            hashset.add(n)
-        return False
+        return len(set(nums)) < len(nums)
     
     # Time Complexity: O(n)
     # Space Complexity: O(n)
