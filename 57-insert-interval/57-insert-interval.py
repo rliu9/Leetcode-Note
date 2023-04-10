6e8f818975972aab7b044f57a2669b6c7f71ff8d
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-04-10 17:29:13 -0400

    Time: 82 ms (68.81%), Space: 17.4 MB (44.61%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,15 +1,16 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
         intervals.append(newInterval)
         intervals.sort(key=lambda x:x[0])
-        res = []
         for interval in intervals:
-            if not res or interval[0] > res[-1][-1]:
+            if not res or interval[0] > res[-1][1]:
                 res.append(interval)
             else:
-                res[-1][-1] = max(res[-1][-1], interval[1])
+                res[-1][-1] = max(interval[1], res[-1][-1])
         return res
-    
+        
+        
     # O(nlogn)
     # O(n)
 if __name__ == '__main__':
