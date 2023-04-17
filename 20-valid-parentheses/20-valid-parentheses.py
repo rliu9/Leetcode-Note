7f1d65dbcd4ae9e9219f93e89c4802b64b9408a4
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-04-17 11:34:11 -0400

    Time: 32 ms (64.73%), Space: 14 MB (15.65%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -3,13 +3,12 @@ class Solution:
         stack = []
         for i in s:
             if i == '(':stack.append(')')
-            elif i == '[':stack.append(']')
             elif i == '{':stack.append('}')
+            elif i == '[':stack.append(']')
             else:
                 if not stack or stack.pop() != i:
                     return False
-        return len(stack) == 0
-    
+        return not stack
     # Time Complexity: O(n)
     # Space Complexity: O(1)
     
