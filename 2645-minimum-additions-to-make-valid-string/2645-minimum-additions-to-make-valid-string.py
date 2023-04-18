d0d569170f636eda5e7fdf7782f0b91f5d4888cc
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-04-18 16:45:54 -0400

    Time: 42 ms (36.04%), Space: 13.8 MB (57.22%) - LeetHub

--- 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.py
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.py
@@ -1,18 +1,21 @@
 class Solution:
     def addMinimum(self, word: str) -> int:
         stack = list(word)
-        i = 0
+        res = 0
         while stack:
             if stack and stack[-1] == 'c':
                 stack.pop()
             else:
-                i += 1
+                res += 1
             if stack and stack[-1] == 'b':
                 stack.pop()
             else:
-                i += 1
+                res += 1
             if stack and stack[-1] == 'a':
                 stack.pop()
             else:
-                i += 1
-        return i
+                res += 1
+        return res
+    
+    # O(n)
+    # O(n)
