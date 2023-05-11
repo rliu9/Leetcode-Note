b5f7af8712851fd5fe3eaf4f2baa469aae954d68
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-05-11 11:54:53 -0400

    Time: 59 ms (76.92%), Space: 16.3 MB (21.09%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,13 +2,17 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         def bt(idx, target, cur, res):
             if target == 0:
-                res.append(cur)
+                res.append(cur[:])
+                return
             if target < 0:return
             for i in range(idx, len(candidates)):
-                bt(i, target-candidates[i], cur+[candidates[i]], res)
+                cur.append(candidates[i])
+                bt(i, target-candidates[i], cur, res)
+                cur.pop()
         res = []
         bt(0, target, [], res)
         return res
+            
     
     # O(2^target)
     # O(max_len_of_combination)
