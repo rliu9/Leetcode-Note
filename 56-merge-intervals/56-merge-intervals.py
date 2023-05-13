d9bb14df6962585196fab9c3af30229ad2845a52
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-05-13 08:12:26 -0400

    Time: 167 ms (16.01%), Space: 21.2 MB (9.69%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,11 +1,18 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda x:x[0])
+        intervals.sort()
         res = []
-        for i in intervals:
-            if not res or res[-1][1] < i[0]:
-                res.append(i)
+        for interval in intervals:
+            if not res or res[-1][-1] < interval[0]:
+                res.append(interval)
             else:
-                res[-1][1] = max(i[1], res[-1][1])
-                
+                res[-1][-1] = max(res[-1][-1], interval[1])
         return res
+    
+    # O(nlogn)
+    # O(n)
+    
+if __name__ == '__main__':
+    s = Solution()
+    assert s.merge([[1,3],[2,6],[8,10],[15,18]]) == [[1,6],[8,10],[15,18]]
+    assert s.merge([[1,4],[4,5]]) == [[1,5]]
