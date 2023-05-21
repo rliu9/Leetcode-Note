a4c3d3b98695e68d53f58c9d46db69962d12c486
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-05-21 17:13:24 -0400

    Time: 44 ms (54.49%), Space: 16.4 MB (23.48%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,16 +1,17 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         row, col = len(matrix), len(matrix[0])
-        def dfs(res, r, c, going_up):
+        def dfs(r, c, going_up, res):
             if 0 <= r < row and 0 <= c < col and matrix[r][c] != '.':
                 res.append(matrix[r][c])
                 matrix[r][c] = '.'
                 if going_up:
-                    dfs(res, r-1, c, True)
-                dfs(res, r, c+1, False)
-                dfs(res, r+1, c, False)
-                dfs(res, r, c-1, False)
-                dfs(res, r-1, c, True)
+                    dfs(r-1, c, going_up, res)
+                dfs(r, c+1, False, res)
+                dfs(r+1, c, False, res)
+                dfs(r, c-1, False, res)
+                dfs(r-1, c, True, res)
         res = []
-        dfs(res, 0, 0, False)
+        dfs(0, 0, True, res)
         return res
+    
