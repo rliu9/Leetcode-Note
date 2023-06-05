fa968e227f67df14c3e4a87c12f59b2226a9d19f
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-06-05 16:24:43 -0400

    Time: 97 ms (12.19%), Space: 16.5 MB (41.07%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -3,11 +3,12 @@ class Solution:
         seen,row,col,color = set(), len(image),len(image[0]),image[sr][sc]
         if color == newColor:return image
         def dfs(r,c,image,newColor):
-            if image[r][c] == color:
-                image[r][c] = newColor
-                if r>=1:dfs(r-1,c,image,newColor)
-                if r+1<row:dfs(r+1,c,image,newColor)
-                if c>=1:dfs(r,c-1,image,newColor)
-                if c+1<col:dfs(r,c+1,image,newColor)
+            if 0 <= r < row and 0 <= c < col:
+                if image[r][c] == color:
+                    image[r][c] = newColor
+                    dfs(r-1,c,image,newColor)
+                    dfs(r+1,c,image,newColor)
+                    dfs(r,c-1,image,newColor)
+                    dfs(r,c+1,image,newColor)
         dfs(sr,sc,image,newColor)
         return image
