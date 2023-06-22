b311d869818ae67198d16677c64eba6b92b1a36c
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-06-22 16:10:09 -0400

    Time: 307 ms (89.60%), Space: 18.8 MB (74.78%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,21 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        row, col, res = len(grid), len(grid[0]), 0
-        def bfs(q):
-            while q:
-                r, c = q.popleft()
-                for x,y in (1,0),(0,1),(-1,0),(0,-1):
-                    rr, cc = x+r, y+c
-                    if 0 <= rr < row and 0 <= cc < col and grid[rr][cc] == '1':
-                        grid[rr][cc] = '0'
-                        q.append((rr,cc))
-                        
+        # dfs
+        row, col = len(grid), len(grid[0])
+        def dfs(r, c):
+            if 0 <= r < row and 0 <= c < col and grid[r][c] == '1':
+                grid[r][c] = '0'
+                dfs(r+1, c)
+                dfs(r-1, c)
+                dfs(r, c+1)
+                dfs(r, c-1)
+        res = 0
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == '1':
-                    bfs(deque([(r,c)]))
+                    dfs(r,c)
                     res += 1
         return res
-    
-    # O(mn)
-    # O(min(m,n))
