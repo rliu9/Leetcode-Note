4473c53fe6b3a846779fd04b5832fbcf87a18798
Rundi Liu <44102063+rliu9@users.noreply.github.com>
2023-07-28 18:13:59 -0400

    Time: 42 ms (80.28%), Space: 16.5 MB (73.24%) - LeetHub

--- 293-flip-game/293-flip-game.py
+++ 293-flip-game/293-flip-game.py
@@ -3,5 +3,5 @@ class Solution:
         res = []
         for i in range(1, len(currentState)):
             if currentState[i-1] == currentState[i] == '+':
-                res.append(currentState[:i-1]+'--'+currentState[i+1:])
+                res.append(currentState[:i-1] + '--' + currentState[i+1:])
         return res
